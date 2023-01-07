16d73844ed21dc9e1318d57093098df0bd3df805
Gaute Lorentsen <gudelo97@gmail.com>
2023-01-07 21:00:59 +0100

    clean up

--- src/components/UserPage/UserPageHeader.js
+++ src/components/UserPage/UserPageHeader.js
@@ -8,12 +8,8 @@ import { Buffer } from 'buffer';
 const UserPageHeader = ({ user, userFriends, commonFriends }) => {
   console.log(user);
   const { id } = useParams();
-  //   const [user, setUser] = useState();
-  //   const [commonFriends, setCommonFriends] = useState();
-  //   const [userFriends, setUserFriends] = useState();
   const [FriendReqBtn, setFriendReqBtn] = useState();
   const [friendPopup, setFriendPopup] = useState();
-  // const [errMsg, setErrMsg] = useState();
 
   const [image, setImage] = useState();
 
@@ -21,45 +17,6 @@ const UserPageHeader = ({ user, userFriends, commonFriends }) => {
   const token = `Bearer ${localStorage.getItem('token')}`;
 
   const hiddenFileInput = useRef(null);
-
-  // useEffect(() => {
-  //   const fetchUserData = async () => {
-  //     const [userResponse, friendList] = await Promise.all([
-  //       fetch(`/home/user/${id}`),
-  //       fetch('/user/getFriends'),
-  //     ]);
-  //     const userData = await userResponse.json();
-  //     const getFriends = await friendList.json();
-
-  //     return [userData, getFriends];
-  //   };
-
-  //   fetchUserData().then((data) => {
-  //     setUser(data[0]);
-  //     setUserFriends(data[1]);
-  //     const equalFriends = data[0].friends.filter((el) => data[1].includes(el));
-  //     setCommonFriends(equalFriends);
-  //   });
-  // }, [id]);
-
-  // useEffect(() => {
-  //   if (user.profilePicture) {
-  //     const base64String = btoa(
-  //       String.fromCharCode(...new Uint8Array(user.profilePicture.data.data))
-  //     );
-
-  //     console.log(user.profilePicture);
-
-  //     const imageUrl = `data:image/jpg;base64,${base64String}`;
-
-  //     console.log(user.profilePicture.data.data.toString('base64'));
-  //     console.log(imageUrl);
-
-  //     setImage(<img src={imageUrl} alt="" />);
-  //   } else {
-  //     setImage(defaultUserPic);
-  //   }
-  // }, [user.profilePicture])
 
   useEffect(() => {
     const addFriend = () => {
